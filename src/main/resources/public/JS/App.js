3bbfc10367e462feded62eb7026f6d1328a14631
Qiao Zhao <victorsterling1@gmail.com>
2016-11-10 22:22:31 -0500

    new version

--- src/main/resources/public/JS/App.js
+++ src/main/resources/public/JS/App.js
@@ -2,33 +2,82 @@
  * Created by victorzhao on 11/5/16.
  */
 'use strict';
-var DCtrl = angular.module('DataService',[]);
+$(function(){
+    var data=[{
+        "Fname" : "Taylor",
+        "Lname" : "Swift",
+        "img" :"../image/sample1.jpg",
+        "gender" : "female",
+        "flower" : "1308",
+        "age" : "36",
+        "note" : "Looking for a handsome gentleman.",
+        "password" : "password1",
+        "email" : "ZEZ7@pitt.edu",
+        "id": 1
+    },{
+        "Fname" : "Yueting",
+        "Lname" : "Lang",
+        "img" :"../image/sample2.jpg",
+        "gender" : "female",
+        "flower" : "1456",
+        "age" : "25",
+        "note" : "Looking for a long-term relationship with a brave and responsible man.",
+        "password" : "password1",
+        "email" : "QIL76@pitt.edu",
+        "id": 2
+    },{
+        "Fname" : "Robert",
+        "Lname" : "Downey",
+        "img" :"../image/sample3.jpg",
+        "gender" : "male",
+        "flower" : "56",
+        "age" : "38",
+        "note" : "Looking for a short-term relationship with a sexy girl.",
+        "password" : "password1",
+        "email" : "LIZ76@pitt.edu",
+        "id": 3
+    }];
 
-DCtrl.controller("DataCtrl", function($scope){
-    var mon = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    var day = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
-    var obj={"Jan":31, "Feb":29, 'Mar':31,'Apr':30,'May':31,'Jun':30,'Jul':31,'Aug':31,'Sep':30,
-        'Oct':31,'Nov':30,'Dec':31};
-    $scope.numLimit = obj[$scope.bmon];
-    $scope.months = mon;
-    $scope.days = day;
-    var getRecentYear = function(){
-        var a = new Date();
-        var year = a.getFullYear();
-        var recentyears = [];
-        for(var i = 0; i < 65; i++) {
-            recentyears.push(year-i);
+    var info = document.getElementById("information");
+
+    function sex(gender){
+
+        if(gender == "all"){
+            return data;
         }
-        return recentyears;
-    };
-    $scope.years = getRecentYear();
+
+        var temp = [];
+
+        for(var i=0;i<data.length;i++){
+            if(data[i].gender===gender){
+                temp.push(data[i]);
+            }
+        }
+
+        return temp;
+
+    }
+
+    for(var i =0;i<data.length;i++){
+        $("<div class='row' id='rowinfo'><div class='col-sm-2 text-center'><img src='"+data[i].img+"' class='img-circle' height='65' width='65' alt='Avatar'></div><div class='col-sm-10'><a href='../view/sampleperson"+data[i].id+".html'>"+data[i].Fname+" "+data[i].Lname+" </a><p>"+data[i].note+"</p></div></div>").appendTo(info);
+    }
 
 
-    $scope.updateDays = function() {
-        $scope.numLimit = obj[$scope.bmon];
-        //console.log($scope.numLimit);
-    };
+    $("#btn").on("click",function(e){
+        if(e.target.nodeName.toLowerCase()==="a"){
+            $('#btn').find("li").removeClass("active");
+            var showData=sex(e.target.getAttribute("id"));
+            $(e.target).parent().addClass("active");
+            info.innerHTML="";
+            for(var i =0;i<showData.length;i++){
+                $("<div class='row' id='rowinfo'><div class='col-sm-2 text-center'><img src='"+showData[i].img+"' class='img-circle' height='65' width='65' alt='Avatar'></div><div class='col-sm-10'><a href='../view/sampleperson"+showData[i].id+".html'>"+showData[i].Fname+" "+data[i].Lname + "</a><p>"+showData[i].note+"</p></div></div>").appendTo(info);
+            }
+        }
+    })
 
 
 
 });
+
+
+
